9e2fc4301d9ada4ae918c403203d45d62d165bdf
Gabriela Melo <gabimelo10@gmail.com>
2017-11-24 11:39:58 -0200

    evertything except take picture handler

--- functions/index.js
+++ functions/index.js
@@ -96,17 +96,95 @@ function validateLocation(location) {
   return buildValidationResult(true, null, null);
 }
 
+function validateLocationAndRelay(location, relay) {
+  const locationTypes = ['bedroom', 'dining room', 'garage', 'kitchen', 'laundry', 'living room'];
+  if (location && locationTypes.indexOf(location.toLowerCase()) === -1) {
+    return buildValidationResult(false, 'location', `We do not have a ${location}. Would you like to try a different location? The most popular location people ask for is the bedroom!`);
+  }
+
+  const relayTypes = ['coffee machine', 'coffeemaker', 'air purifier', 'light', 'humidifier', 'heater', 'air conditioner'];
+  if (relay && relayTypes.indexOf(relay.toLowerCase()) === -1) {
+    return buildValidationResult(false, 'relay', `We do not have a ${relay}. Would you like to try a different relay? The most popular relay people ask for is the light!`);
+  }
+
+  return buildValidationResult(true, null, null);
+}
+
+function validateLocationAndState(location, state) {
+  const locationTypes = ['bedroom', 'dining room', 'garage', 'kitchen', 'laundry', 'living room'];
+  if (location && locationTypes.indexOf(location.toLowerCase()) === -1) {
+    return buildValidationResult(false, 'location', `We do not have a ${location}. Would you like to try a different location? The most popular location people ask for is the bedroom!`);
+  }
+
+  const stateTypes = ['on', 'off'];
+  if (state && stateTypes.indexOf(state.toLowerCase()) === -1) {
+    return buildValidationResult(false, 'state', `${state} is not a valid command. You can turn it on or off`);
+  }
+
+  return buildValidationResult(true, null, null);
+}
+
+function validateLocationAndRelayAndStatus(location, relay, status) {
+  const locationTypes = ['bedroom', 'dining room', 'garage', 'kitchen', 'laundry', 'living room'];
+  if (location && locationTypes.indexOf(location.toLowerCase()) === -1) {
+    return buildValidationResult(false, 'location', `We do not have a ${location}. Would you like to try a different location? The most popular location people ask for is the bedroom!`);
+  }
+
+  const relayTypes = ['coffee machine', 'coffeemaker', 'air purifier', 'light', 'humidifier', 'heater', 'air conditioner'];
+  if (relay && relayTypes.indexOf(relay.toLowerCase()) === -1) {
+    return buildValidationResult(false, 'relay', `We do not have a ${relay}. Would you like to try a different relay? The most popular relay people ask for is the light!`);
+  }
+
+  const statusTypes = ['on', 'off'];
+  if (status && statusTypes.indexOf(status.toLowerCase()) === -1) {
+    return buildValidationResult(false, 'status', `${status} is not a valid command. You can turn it on or off`);
+  }
+
+  return buildValidationResult(true, null, null);
+}
+
+function validateLocationAndRelayAndSensor(location, relay, sensor) {
+  const locationTypes = ['bedroom', 'dining room', 'garage', 'kitchen', 'laundry', 'living room'];
+  if (location && locationTypes.indexOf(location.toLowerCase()) === -1) {
+    return buildValidationResult(false, 'location', `We do not have a ${location}. Would you like to try a different location? The most popular location people ask for is the bedroom!`);
+  }
+
+  const relayTypes = ['coffee machine', 'coffeemaker', 'air purifier', 'light', 'humidifier', 'heater', 'air conditioner'];
+  if (relay && relayTypes.indexOf(relay.toLowerCase()) === -1) {
+    return buildValidationResult(false, 'relay', `We do not have a ${relay}. Would you like to try a different relay? The most popular relay people ask for is the light!`);
+  }
+
+  const sensorTypes = ['presence', 'opening', 'none'];
+  if (sensor && sensorTypes.indexOf(sensor.toLowerCase()) === -1) {
+    return buildValidationResult(false, 'sensor', `We do not have a ${sensor} sensor. We currently support opening or presence sensors!`);
+  }
+
+  return buildValidationResult(true, null, null);
+}
+
 function mockAllStatus() {
   return {
     'luminosity': (200 + Math.random() * 500.0).toFixed(1),
     'humidity': (20.0 + Math.random() * 70.0).toFixed(1),
-    'opening': Math.random() < 0.5 ? 'on' : 'off',
-    'presence': Math.random() < 0.5 ? 'on' : 'off',
-    'relay1': Math.random() < 0.5 ? 'on' : 'off',
-    'relay2': Math.random() < 0.5 ? 'on' : 'off',
+    'opening': Math.random() < 0.5 ? 'open' : 'closed',
+    'presence': Math.random() < 0.5 ? true : false,
+    'relay': Math.random() < 0.5 ? 'on' : 'off',
     'temperature': (15.0 + Math.random() * 15.0).toFixed(1),
     'alarm': Math.random() < 0.5 ? 'on' : 'off',
   };
+}
+
+function mockPicture(location) {
+  locationPictures = {
+    'bedroom': 'https://i.pinimg.com/736x/02/f5/7a/02f57a648ee803312085cf3676a6f79b--blogger-bedroom-bedroom-inspo.jpg', 
+    // TODO (if its really gonna be this way)
+    // 'dining room': ,
+    // 'garage': ,
+    // 'kitchen': ,
+    // 'laundry': ,
+    // 'living room': 
+  }
+  return locationPictures[location]
 }
 
 // --------------- Functions that control the bot's behavior -----------------------
@@ -165,47 +243,170 @@ function allSensorStatusHandler(intentRequest, callback) {
       return;
     }
 
-    // Pass the price of the flowers back through session attributes to be used in various prompts defined on the bot model.
     const outputSessionAttributes = intentRequest.sessionAttributes || {};
-    const mockedAllStatus = mockAllStatus();
-
-    if (location) {
-      outputSessionAttributes.Status = `This is the status for ${location}: ${JSON.stringify(mockedAllStatus)}`;
-    }
 
     callback(delegate(outputSessionAttributes, intentRequest.currentIntent.slots));
     return;
   }
 
-  const status = intentRequest.sessionAttributes.status;
-//   console.log('status: ', status)
-  // Order the flowers, and rely on the goodbye message of the bot to define the message to the end user.  In a real bot, this would likely involve a call to a backend service.
   callback(close(intentRequest.sessionAttributes, 'Fulfilled',
-  { contentType: 'PlainText', content: `This is the status for ${location}: ${JSON.stringify(status)}` }));
+  { contentType: 'PlainText', content: `This is the status for ${location}: ${JSON.stringify(mockAllStatus())}` }));
 }
 
 function humiditySensorStatus(intentRequest, callback) {
-  // TODO
+  const location = intentRequest.currentIntent.slots.location;
+  const source = intentRequest.invocationSource;
+
+  if (source === 'DialogCodeHook') {
+    const slots = intentRequest.currentIntent.slots;
+
+    const validationResult = validateLocation(location);
+    if (!validationResult.isValid) {
+      slots[`${validationResult.violatedSlot}`] = null;
+      callback(elicitSlot(intentRequest.sessionAttributes, intentRequest.currentIntent.name, slots, validationResult.violatedSlot, validationResult.message));
+      return;
+    }
+
+    const outputSessionAttributes = intentRequest.sessionAttributes || {};
+
+    callback(delegate(outputSessionAttributes, intentRequest.currentIntent.slots));
+    return;
+  }
+
+  callback(close(intentRequest.sessionAttributes, 'Fulfilled',
+  { contentType: 'PlainText', content: `This is the humidity for ${location}: ${mockAllStatus().humidity}` }));
 }
 
 function openingSensorStatus(intentRequest, callback) {
-  // TODO
+  const location = intentRequest.currentIntent.slots.location;
+  const source = intentRequest.invocationSource;
+
+  if (source === 'DialogCodeHook') {
+    const slots = intentRequest.currentIntent.slots;
+
+    const validationResult = validateLocation(location);
+    if (!validationResult.isValid) {
+      slots[`${validationResult.violatedSlot}`] = null;
+      callback(elicitSlot(intentRequest.sessionAttributes, intentRequest.currentIntent.name, slots, validationResult.violatedSlot, validationResult.message));
+      return;
+    }
+
+    const outputSessionAttributes = intentRequest.sessionAttributes || {};
+
+    callback(delegate(outputSessionAttributes, intentRequest.currentIntent.slots));
+    return;
+  }
+
+  callback(close(intentRequest.sessionAttributes, 'Fulfilled',
+  { contentType: 'PlainText', content: `It is ${mockAllStatus().opening}` }));
 }
 
 function presenceSensorStatus(intentRequest, callback) {
-  // TODO
+  const location = intentRequest.currentIntent.slots.location;
+  const source = intentRequest.invocationSource;
+
+  if (source === 'DialogCodeHook') {
+    const slots = intentRequest.currentIntent.slots;
+
+    const validationResult = validateLocation(location);
+    if (!validationResult.isValid) {
+      slots[`${validationResult.violatedSlot}`] = null;
+      callback(elicitSlot(intentRequest.sessionAttributes, intentRequest.currentIntent.name, slots, validationResult.violatedSlot, validationResult.message));
+      return;
+    }
+
+    const outputSessionAttributes = intentRequest.sessionAttributes || {};
+
+    callback(delegate(outputSessionAttributes, intentRequest.currentIntent.slots));
+    return;
+  }
+
+  callback(close(intentRequest.sessionAttributes, 'Fulfilled',
+  { contentType: 'PlainText', content: `There is ${mockAllStatus().presence ? 'someone' : 'no one'} in ${location}` }));
 }
 
 function relayStatus(intentRequest, callback) {
-  // TODO
+  const location = intentRequest.currentIntent.slots.location;
+  const relay = intentRequest.currentIntent.slots.relay;
+  const source = intentRequest.invocationSource;
+
+  if (source === 'DialogCodeHook') {
+    const slots = intentRequest.currentIntent.slots;
+
+    const validationResult = validateLocationAndRelay(location, relay);
+    if (!validationResult.isValid) {
+      slots[`${validationResult.violatedSlot}`] = null;
+      callback(elicitSlot(intentRequest.sessionAttributes, intentRequest.currentIntent.name, slots, validationResult.violatedSlot, validationResult.message));
+      return;
+    }
+
+    const outputSessionAttributes = intentRequest.sessionAttributes || {};
+
+    callback(delegate(outputSessionAttributes, intentRequest.currentIntent.slots));
+    return;
+  }
+
+  callback(close(intentRequest.sessionAttributes, 'Fulfilled',
+  { contentType: 'PlainText', content: `The ${relay} is ${mockAllStatus().relay} in ${location}` }));
 }
 
 function setAlarm(intentRequest, callback) {
-  // TODO
+  const location = intentRequest.currentIntent.slots.location;
+  const state = intentRequest.currentIntent.slots.state;
+  const source = intentRequest.invocationSource;
+
+  if (source === 'DialogCodeHook') {
+    const slots = intentRequest.currentIntent.slots;
+
+    const validationResult = validateLocationAndState(location, state);
+    if (!validationResult.isValid) {
+      slots[`${validationResult.violatedSlot}`] = null;
+      callback(elicitSlot(intentRequest.sessionAttributes, intentRequest.currentIntent.name, slots, validationResult.violatedSlot, validationResult.message));
+      return;
+    }
+
+    const outputSessionAttributes = intentRequest.sessionAttributes || {};
+
+    callback(delegate(outputSessionAttributes, intentRequest.currentIntent.slots));
+    return;
+  }
+
+  callback(close(intentRequest.sessionAttributes, 'Fulfilled',
+  { contentType: 'PlainText', content: `The alarm in ${location} was turned ${state}` }));
 }
 
 function setAutoRelay(intentRequest, callback) {
-  // TODO
+  const location = intentRequest.currentIntent.slots.location;
+  const relay = intentRequest.currentIntent.slots.relay;
+  const sensor = intentRequest.currentIntent.slots.sensor;
+  const startTime = intentRequest.currentIntent.slots.startTime;
+  const endTime = intentRequest.currentIntent.slots.endTime;
+  const autoKeepOn = intentRequest.currentIntent.slots.autoKeepOn;
+  const source = intentRequest.invocationSource;
+
+  if (source === 'DialogCodeHook') {
+    const slots = intentRequest.currentIntent.slots;
+
+    const validationResult = validateLocationAndRelayAndSensor(location, relay, sensor);
+    if (!validationResult.isValid) {
+      slots[`${validationResult.violatedSlot}`] = null;
+      callback(elicitSlot(intentRequest.sessionAttributes, intentRequest.currentIntent.name, slots, validationResult.violatedSlot, validationResult.message));
+      return;
+    }
+
+    const outputSessionAttributes = intentRequest.sessionAttributes || {};
+
+    callback(delegate(outputSessionAttributes, intentRequest.currentIntent.slots));
+    return;
+  }
+
+  let sensorString = ''
+  if (sensor != 'none') {
+    sensorString = `when the ${sensor} is triggered`
+  }
+  callback(close(intentRequest.sessionAttributes, 'Fulfilled',
+  { contentType: 'PlainText', content: `The ${location} ${relay} will be turned on ${sensorString}` +
+  `, between ${startTime} and ${endTime}, and will remain on for ${autoKeepOn}  minutes.` }));
 }
 
 function takePicture(intentRequest, callback) {
@@ -213,11 +414,53 @@ function takePicture(intentRequest, callback) {
 }
 
 function temperatureSensorStatus(intentRequest, callback) {
-  // TODO
+  const location = intentRequest.currentIntent.slots.location;
+  const source = intentRequest.invocationSource;
+
+  if (source === 'DialogCodeHook') {
+    const slots = intentRequest.currentIntent.slots;
+
+    const validationResult = validateLocation(location);
+    if (!validationResult.isValid) {
+      slots[`${validationResult.violatedSlot}`] = null;
+      callback(elicitSlot(intentRequest.sessionAttributes, intentRequest.currentIntent.name, slots, validationResult.violatedSlot, validationResult.message));
+      return;
+    }
+
+    const outputSessionAttributes = intentRequest.sessionAttributes || {};
+
+    callback(delegate(outputSessionAttributes, intentRequest.currentIntent.slots));
+    return;
+  }
+
+  callback(close(intentRequest.sessionAttributes, 'Fulfilled',
+  { contentType: 'PlainText', content: `This is the temperature for ${location}: ${mockAllStatus().temperature}` }));
 }
 
 function toggleRelay(intentRequest, callback) {
-  // TODO
+  const location = intentRequest.currentIntent.slots.location;
+  const relay = intentRequest.currentIntent.slots.relay;
+  const status = intentRequest.currentIntent.slots.status;
+  const source = intentRequest.invocationSource;
+
+  if (source === 'DialogCodeHook') {
+    const slots = intentRequest.currentIntent.slots;
+
+    const validationResult = validateLocationAndRelayAndStatus(location, relay, status);
+    if (!validationResult.isValid) {
+      slots[`${validationResult.violatedSlot}`] = null;
+      callback(elicitSlot(intentRequest.sessionAttributes, intentRequest.currentIntent.name, slots, validationResult.violatedSlot, validationResult.message));
+      return;
+    }
+
+    const outputSessionAttributes = intentRequest.sessionAttributes || {};
+
+    callback(delegate(outputSessionAttributes, intentRequest.currentIntent.slots));
+    return;
+  }
+
+  callback(close(intentRequest.sessionAttributes, 'Fulfilled',
+  { contentType: 'PlainText', content: `The ${relay} was turned ${status} in ${location}` }));
 }
 
 // --------------- Intents -----------------------
